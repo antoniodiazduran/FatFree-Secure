ca7f3c2843381315eecb8a9b4b44700a20eae3e8
Antonio Diaz <antoniodiazduran@gmail.com>
2020-07-19 08:50:53 -0400

    adding controller class

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -106,8 +106,9 @@
                     $this->f3->set('stat','success');
                     $this->f3->set('msg','Welcome!');
                     $this->f3->set('view','main.htm');
+                    $sMail = new Controller;
                     $msg = 'Username is:'.$user->username.' granted as '.$user->roles.' with '.$user->company.' click on the link ';
-                    sendMail('antoniodiazduran@yahoo.com',$msg);
+                    $sMail->sendMail('antoniodiazduran@yahoo.com',$msg);
                 } else {
                     $this->f3->set('stat','danger');
                     $this->f3->set('msg','Incorrect Username & Password');
